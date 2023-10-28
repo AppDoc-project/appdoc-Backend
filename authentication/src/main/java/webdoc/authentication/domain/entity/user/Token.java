6f5bb111f2df0db92a2904fe3ee9b22992658d8e
fearlessdeveloper <1dilumn0@gmail.com>
2023-10-28 20:29:39 +0900

    [HOTFIX] Token 객체 nullable수정

--- authentication/src/main/java/webdoc/authentication/domain/entity/user/Token.java
+++ authentication/src/main/java/webdoc/authentication/domain/entity/user/Token.java
@@ -28,12 +28,14 @@ public class Token {
     private Long id;
 
     // 토큰 유효 기간 지정
+    @Column(nullable = false)
     private LocalDateTime expiredAt;
 
     // 중복 로그인을 차단하기 위한 최소한의 조치
+    @Column(nullable = false)
     private String value;
 
     @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn
+    @JoinColumn(nullable = false)
     private User user;
 }
