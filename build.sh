0748a6c611a5af5fea9a49e5ca51a1651ca3157e
fearlessdeveloper <1dilumn0@gmail.com>
2023-11-19 02:15:46 +0900

    [FEAT] 배포 설정 수정

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
-# 개발 환경용 docker-compose 파일
+# docker push
 cd ./authentication
 ./gradlew clean build
-
+docker push cualestunombre/back:authentication
 cd ..
+
+
 cd ./community
 ./gradlew clean build
+docker push cualestunombre/back:community
+cd ..
+
+cd ./nginx
+docker push cualestunombre/back:nginx
+cd ..
+
+cd ./mysql
+docker push cualestunombre/back:mysql
+cd ..
 
 
-docker-compose up --build
